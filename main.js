049accc53f1e85db879e960a199e518ea8ac425e
Sriharish-947 <74801176+Sriharish-947@users.noreply.github.com>
2021-12-27 10:12:28 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+LwristX = 0;
+RwristX = 0;
+difference = 0;
 function setup()
 {
     video = createCapture(VIDEO);
@@ -17,17 +20,25 @@ function modelLoaded()
     console.log("poseNet, ACTIVATE!");
 }
 
+
 function gotPoses(result)
 {
     if(result.length > 0)
     {
         console.log(result);
+        LwristX = result[0].pose.leftWrist.x;
+        RwristX = result[0].pose.rightWrist.x;
+        difference = floor(LwristX - RwristX);
+        console.log("difference is = " + difference);
     }
 }
 
 function draw()
 {
-    
+    background("#969A97");
+    textSize(difference);
+    fill("white");
+    text("Harish", 0, 300);
 }
 
 /*function setup()
@@ -54,4 +65,21 @@ function gotPoses(result)
     {
         console.log(result);
     }
-}*/
+}
+
+function gotPoses(result)
+{
+    if(result.length > 0)
+    {
+        console.log(result);
+    }
+}
+
+
+ noseX = result[0].pose.nose.x;
+        console.log("Nose X = " + noseX);   
+        noseY = result[0].pose.nose.y;
+        console.log("Nose Y = " + noseY);  
+
+
+*/
